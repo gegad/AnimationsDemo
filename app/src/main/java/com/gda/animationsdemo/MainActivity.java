a3cc8d2473b47c0c65f76a0f743a9e9544e5fa2d
geghetsikd <geghetsikd@gmail.com>
2019-09-23 18:52:05 +0400

    Added more animations and  loadAnimation method.

--- app/src/main/java/com/gda/animationsdemo/MainActivity.java
+++ app/src/main/java/com/gda/animationsdemo/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SeekBar;
@@ -14,11 +15,19 @@ public class MainActivity extends AppCompatActivity {
     Animation animFadeIn;
     Animation animFadeOut;
     Animation animFadeInOut;
+
     Animation animZoomIn;
     Animation animZoomOut;
+
     Animation animLeftRight;
     Animation animRightLeft;
     Animation animTopBot;
+
+    Animation animBounce;
+    Animation animFlash;
+
+    Animation animRotateLeft;
+    Animation animRotateRight;
 
     ImageView mImageView;
     TextView mTextView;
@@ -31,6 +40,10 @@ public class MainActivity extends AppCompatActivity {
     Button btnLeftRight;
     Button btnRightLeft;
     Button btnTopBot;
+    Button btnBounce;
+    Button btnFlash;
+    Button btnRotateLeft;
+    Button btnRotateRight;
 
     SeekBar mSeekBarSpeed;
     TextView mTextSpeed;
@@ -44,5 +57,25 @@ public class MainActivity extends AppCompatActivity {
         loadAnimations();
         loadUI();
 
+    }
+
+    private void loadAnimations() {
+        animFadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        animFadeIn.setAnimationListener(this);
+        animFadeOut = AnimationUtils.loadAnimation(this, R.anim.fade_out);
+        animFadeInOut  = AnimationUtils.loadAnimation(this, R.anim.fade_in_out);
+
+        animZoomIn = AnimationUtils.loadAnimation(this, R.anim.zoom_in);
+        animZoomOut = AnimationUtils.loadAnimation(this, R.anim.zoom_out);
+
+        animLeftRight = AnimationUtils.loadAnimation(this, R.anim.left_right);
+        animRightLeft = AnimationUtils.loadAnimation(this, R.anim.right_left);
+        animTopBot = AnimationUtils.loadAnimation(this, R.anim.top_bot);
+
+        animBounce = AnimationUtils.loadAnimation(this, R.anim.bounce);
+        animFlash = AnimationUtils.loadAnimation(this, R.anim.flash);
+
+        animRotateLeft = AnimationUtils.loadAnimation(this, R.anim.rotate_left);
+        animRotateRight = AnimationUtils.loadAnimation(this, R.anim.rotate_right);
     }
 }
